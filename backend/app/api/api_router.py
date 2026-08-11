2216ea33793ee482ddb1f0bd9cc6a379a4d90dc4
Davie Karanja <oonlinegigz@gmail.com>
2026-08-12 02:09:24 +0300

    disables auth fo management route temporarily

--- backend/app/api/api_router.py
+++ backend/app/api/api_router.py
@@ -10,7 +10,9 @@ api_router = APIRouter(prefix='/api/v1')
 
 if settings.admin_route:
     logger.info("Admin route is enabled. Including management routes.")
-    api_router.include_router(management.router, prefix="/management", dependencies=[Depends(get_current_staff)], tags=["Management"])
+    api_router.include_router(
+        management.router, prefix="/management", tags=["Management"]
+    ),  # dependencies=[Depends(get_current_staff)],
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(organization.router, prefix="/organizations", tags=["Organization Management"])
@@ -20,4 +22,3 @@ api_router.include_router(products.router, prefix="/products", tags=["products"]
 # api_router.include_router(payments.router, prefix="/payments", tags=["payments"])
 # api_router.include_router(checkout.router, prefix="/terminal", tags=["Checkout Pipeline"])
 # api_router.include_router(staff.router, prefix="/staff", tags=["Staff Management"])
-
